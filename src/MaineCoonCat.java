ad3d9a7a594d4633c75828ba98b8d8503e59db05
Kanat <kantbeka@gmail.com>
2019-01-10 02:53:33 +0600

    cat commit

---
+++ src/MaineCoonCat.java
@@ -0,0 +1,11 @@
+public class MaineCoonCat extends Cat {
+    @Override
+    public void display() {
+        System.out.println("Maine coon cat is displayed, it is very big");
+    }
+
+    @Override
+    public void jump() {
+        System.out.println("How does this cat jump? - Jumps low");
+    }
+}
